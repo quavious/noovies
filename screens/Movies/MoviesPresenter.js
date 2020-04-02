09add7f007e4b66835291e1eedbbf22f1df1a601
quavious <leenowon97@gmail.com>
2020-04-02 21:01:00 +0900

    slider added

--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -1,42 +1,40 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import Swiper from 'react-native-web-swiper';
-import { Dimensions, ActivityIndicator } from 'react-native';
+import { Dimensions, ActivityIndicator,View } from 'react-native';
+import Slide from '../../components/Movies/Slide';
 
-const {width, height} = Dimensions.get("screen")
-
-const Header = styled.View`
-    width: 100%;
-    height: ${height/3}px;
-`
-
-const Text = styled.Text``
-
-const Section = styled.View`
-    background-color: red;
-    height: 100%;
-`
+const {width: WIDTH, height: HEIGHT} = Dimensions.get("screen")
 
 const Container = styled.View`
     flex: 1;
     background-color: black;
     justify-content: center;
 `
-
+const SliderContainer = styled.View`
+    width: ${WIDTH}px;
+    height : ${HEIGHT/4}px;
+`
 
 export default ({ loading, nowPlaying }) => {
     return (
         <Container>
             {loading ? <ActivityIndicator color="white" size="small"/> : (
-            <Header>
+            <>
+            <SliderContainer>
                 <Swiper controlsEnabled={false} loop timeout={5}>
                     {nowPlaying.map(movie => (
-                        <Section key={movie.id}>
-                            <Text>{movie.original_title}</Text>
-                        </Section>
+                        <Slide 
+                            key={movie.id} 
+                            id={movie.id} 
+                            title={movie.original_title}
+                            overview={movie.overview}
+                            votes={movie.vote_average}
+                            backgroundImage={movie.backdrop_path} />
                     ))}
                 </Swiper>
-            </Header>
+            </SliderContainer>
+            </>
             )}
         </Container>    
     )
